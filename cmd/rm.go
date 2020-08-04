9ace4e9b1737b635df1ea450cec999d126109bbe
haydenjeune <33794706+haydenjeune@users.noreply.github.com>
2020-08-05 00:02:09 +1200

    Add command to remove existing profiles

---
+++ cmd/rm.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/haydenjeune/committer/pkg/profile"
+	"github.com/spf13/cobra"
+)
+
+// rmCmd handles the `committer rm` command
+var rmCmd = &cobra.Command{
+	Use:   "rm",
+	Short: "Remove a saved committer profile",
+	Run: func(cmd *cobra.Command, args []string) {
+		profileName := args[0]
+
+		profileStore := profile.NewStorage(osfs.New(committerConfigDir()))
+		profiles, err := profileStore.Read()
+		if err != nil {
+			fmt.Printf("error reading profiles: %v\n", err)
+		}
+
+		if _, exists := profiles[profileName]; !exists {
+			return
+		}
+
+		delete(profiles, profileName)
+		if err := profileStore.Save(profiles); err != nil {
+			fmt.Printf("error deleting profile: %v\n", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(rmCmd)
+}
